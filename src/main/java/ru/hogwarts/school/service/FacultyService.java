0c893f4842a02578b695013cbc4cd400798253e6
aLexa163-JV <alekcei-y@ya.ru>
2025-01-25 14:39:34 +0400

    Введение в базы данных

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,39 @@
+package ru.hogwarts.school.service;
+
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repositories.FacultyRepository;
+
+import java.util.Collection;
+
+@Service
+public class FacultyService {
+
+
+    private FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty addFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty findFaculty(Long id) {
+        return facultyRepository.findById(id).get();
+    }
+
+    public Faculty editFaculty(Long id, Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(Long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public Collection<Faculty> getAllFaculties() {
+        return facultyRepository.findAll();
+    }
+}
